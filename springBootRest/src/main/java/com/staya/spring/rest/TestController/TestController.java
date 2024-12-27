ee7370ba249a1704061b35c5e9db992d1aeb2073
surya <suryaprakashnarayana@gmail.com>
2024-12-27 15:45:56 +0530

    nah

--- springBootRest/src/main/java/com/staya/spring/rest/TestController/TestController.java
+++ springBootRest/src/main/java/com/staya/spring/rest/TestController/TestController.java
@@ -1,7 +1,6 @@
-package com.staya.spring.rest.TestController;
+package com.staya.spring.rest.testcontroller;
 
 import org.springframework.http.HttpStatus;
-//import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,5 +23,6 @@ public class TestController {
                               .header("status","surya sucess")
                               .body(response.getBody());
 	}
+
 
 }
